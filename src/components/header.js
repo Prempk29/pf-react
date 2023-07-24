92eb733eb6f337c75cf59734879305d8fd268ad6
Tadi Prem Kumar Naidu <prempk.7172@gmail.com>
2023-07-24 23:26:48 +0530

    Add files via upload

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "../App.css"
+function Header(){
+return (
+    <div className="prem">
+        <p>Prem Kumar</p>
+        <div class="social-menu">
+        <ul>
+            <li><a href="https://github.com/Prempk29" target="blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/tadi-prem-kumar-naidu-b47179237/" target="blank"><i class="fab fa-linkedin-in"></i></a></li>
+            <li><a href="https://app.luminpdf.com/viewer/64b819a87ee1eeed36798a83" target="blank"><i class="fa fa-file-text" aria-hidden="true" id="p"></i></a></li>
+            
+        </ul>
+    </div>
+    </div>
+)
+}
+export default Header;
